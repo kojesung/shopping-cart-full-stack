b0421fdecd66948effbc39e6887d7f8b56315032
kojesung <js95112345@gmail.com>
2026-06-02 15:28:22 +0900

    chore: vite+jest 조합 설정

---
+++ client/jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+    testEnvironment: 'jsdom',
+    setupFilesAfterFramework: ['@testing-library/jest-dom'],
+    transform: {
+        '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+    },
+    moduleNameMapper: {
+        '\\.(css|less|scss)$': 'identity-obj-proxy',
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+};

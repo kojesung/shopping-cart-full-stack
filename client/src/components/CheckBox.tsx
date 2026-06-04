d97491d149adc7490586c7f38d315fa99fd2303f
kojesung <js95112345@gmail.com>
2026-06-04 23:30:42 +0900

    fix: CheckBox 컴포넌트 배경색 수정

--- client/src/components/CheckBox.tsx
+++ client/src/components/CheckBox.tsx
@@ -8,6 +8,7 @@ interface CheckBoxProps {
 }
 
 // 만들다보니 그냥 배경까지 포함한 이미지를 조건부로 보여주면 될 것 같은데, 직접 구현 방식이 귀찮기도 하고 체크 아이콘과 배경 상태를 따로 동기화해주기 때문에 더 불안정한 느낌
+// 그리고 생각해보니 input으로 해야함;;
 export default function CheckBox({ checkStatus, handleCheckClick }: CheckBoxProps) {
     return (
         <CheckBoxContainer checkStatus={checkStatus} onClick={handleCheckClick}>
@@ -24,7 +25,7 @@ const CheckBoxContainer = styled.button<CheckBoxContainerProps>`
     width: 24px;
     height: 24px;
     border-radius: 8px;
-    background-color: ${(props) => (props.checkStatus ? '#0000001A' : '#ffffff')};
+    background-color: ${(props) => (props.checkStatus ? '#000000' : '#ffffff')};
     border: ${(props) => (props.checkStatus ? 'none' : '1px solid #0000001A')};
     display: flex;
     justify-content: center;

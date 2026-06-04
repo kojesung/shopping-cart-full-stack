2951728a0d9764eb5167cc5a5c9473d24f682e54
kojesung <js95112345@gmail.com>
2026-06-05 00:04:45 +0900

    fix: ItemActionButton 컴포넌트에 onClick 함수 전달

--- client/src/components/ItemActionButton.tsx
+++ client/src/components/ItemActionButton.tsx
@@ -12,8 +12,8 @@ type NodeChildren = {
 
 type ItemActionButtonProps = (TextChildren | NodeChildren) & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function ItemActionButton({ text, children }: ItemActionButtonProps) {
-    return <ItemActionButtonContainer>{text ? text : children}</ItemActionButtonContainer>;
+export default function ItemActionButton({ text, children, onClick }: ItemActionButtonProps) {
+    return <ItemActionButtonContainer onClick={onClick}>{text ? text : children}</ItemActionButtonContainer>;
 }
 
 const ItemActionButtonContainer = styled.button`

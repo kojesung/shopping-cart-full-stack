8e41e626f9b8c80dcb71160b3c6295c2e6792c91
kojesung <js95112345@gmail.com>
2026-06-04 20:36:22 +0900

    feat: 전체 장바구니를 다루기 위한 useCart 훅 구현

---
+++ client/src/hooks/useCart.ts
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import { optimisticUpdate } from '../optimisticUpdate';
+import { useCheckboxStatus } from './useCheckboxStatus';
+import { useQuantityStatus } from './useQuantityStatus';
+import { cartApiService, type Product } from '../api/cartApiService';
+
+export const useCart = () => {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [apiStatus, setApiStatus] = useState<'idle' | 'loading' | 'success' | 'error'>('idle');
+
+    const { checkStatus, toggle: handleToggle, remove: removeCheck, initCheckStatus } = useCheckboxStatus(3);
+    const {
+        quantityStatus,
+        increase,
+        decrease,
+        remove: removeQuantity,
+        initQuantityStatus,
+    } = useQuantityStatus([1, 2, 3]);
+
+    useEffect(() => {
+        const fetchCart = async () => {
+            setApiStatus('loading');
+            try {
+                const data = await cartApiService.getCart(1);
+                setProducts(data.data.products);
+                initCheckStatus(data.data.products.length);
+                initQuantityStatus(data.data.products.map((p) => p.quantity));
+                setApiStatus('success');
+            } catch {
+                setApiStatus('error');
+            }
+        };
+
+        fetchCart();
+    }, [initCheckStatus, initQuantityStatus]);
+
+    const handleIncrease = (index: number) => {
+        optimisticUpdate({
+            apiCallFn: () => cartApiService.updateCartProductQuantity(1, products[index].id, quantityStatus[index] + 1),
+            onSuccess: () => increase(index),
+            onError: () => decrease(index),
+        });
+    };
+
+    const handleDecrease = (index: number) => {
+        optimisticUpdate({
+            apiCallFn: () => cartApiService.updateCartProductQuantity(1, products[index].id, quantityStatus[index] - 1),
+            onSuccess: () => decrease(index),
+            onError: () => increase(index),
+        });
+    };
+
+    const remove = (index: number) => {
+        removeCheck(index);
+        removeQuantity(index);
+    };
+    return { handleIncrease, handleDecrease, handleToggle, remove, checkStatus, apiStatus };
+};

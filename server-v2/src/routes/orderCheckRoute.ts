9e20a7451af0b9041cdf6cb149dcd9b32e41b745
goodsmell <87801306+goodsmell@users.noreply.github.com>
2026-06-18 23:50:38 +0900

    feat: 주문확인 쿠폰 API 연결
    Co-authored-by: kojesung <kojesung@users.noreply.github.com>

--- server-v2/src/routes/orderCheckRoute.ts
+++ server-v2/src/routes/orderCheckRoute.ts
@@ -4,6 +4,9 @@ import {
   getOrderCheck,
   getOrderCheckPayInfo,
   patchRemoteAreaSelection,
+  getOrderCheckCoupons,
+  patchOrderCheckCoupons,
+  postOrderCheckCouponDiscount,
 } from '../controllers/OrderCheckController.js';
 
 const orderCheckRouter = Router();
@@ -12,5 +15,8 @@ orderCheckRouter.post('/order-check', postOrderCheck);
 orderCheckRouter.get('/order-check', getOrderCheck);
 orderCheckRouter.get('/order-check/pay-info', getOrderCheckPayInfo);
 orderCheckRouter.patch('/order-check/select/remote-areas', patchRemoteAreaSelection);
+orderCheckRouter.get('/order-check/coupons', getOrderCheckCoupons);
+orderCheckRouter.patch('/order-check/coupons', patchOrderCheckCoupons);
+orderCheckRouter.post('/order-check/coupons', postOrderCheckCouponDiscount);
 
 export default orderCheckRouter;

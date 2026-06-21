2d16db39d18bd8b4669970e4ada738fc91f573de
kojesung <js95112345@gmail.com>
2026-06-21 15:53:40 +0900

    feat: Modal 관리 context 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CartPage from './pages/CartPage';
 import OrderConfirmPage from './pages/OrderConfirmPage';
+import { ModalProvider } from './components/modal/ModalProvider';
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<CartPage />} />
-                <Route path="/order-confirm" element={<OrderConfirmPage />} />
-            </Routes>
+            <ModalProvider>
+                <Routes>
+                    <Route path="/" element={<CartPage />} />
+                    <Route path="/order-confirm" element={<OrderConfirmPage />} />
+                </Routes>
+            </ModalProvider>
         </BrowserRouter>
     );
 }
